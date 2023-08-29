96c09d3502dcd24420f64a4f341f93c283613b6e
Sukham Singh <124015833+sukham23062004@users.noreply.github.com>
2023-08-29 23:15:07 +0530

    Add files via upload

---
+++ public/form4.js
@@ -0,0 +1,67 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycby5C-Pliow8aopogfXJKeKYiTcKlN1rymsFPx1k7chTlOmrvKwYdwcQ83PL0CIphhXP8w/exec";
+const form = document.forms["submit-to-google-sheet"];
+const loadingScreen1 = document.querySelector(".loading-container1");
+const loadingScreen2 = document.querySelector(".loading-container2");
+const SuccessPage = document.querySelector(".SuccessPage");
+const reEnter = document.querySelector(".reEnter");
+const form_cont = document.querySelector(".form_wrapper");
+const teamname = document.querySelector("[data-teamname]");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  SubmitData();
+});
+async function SubmitData() {
+  form_cont.classList.add("None");
+  loadingScreen2.classList.add("active");
+  try {
+    const check = await fetch(
+      "https://script.google.com/macros/s/AKfycby5C-Pliow8aopogfXJKeKYiTcKlN1rymsFPx1k7chTlOmrvKwYdwcQ83PL0CIphhXP8w/exec?action=getData"
+    );
+    const checkResult = await check.json();
+    let checked = 1;
+    for (var i = 0; i < checkResult.length; i++) {
+      if (teamname.value.toUpperCase() == checkResult[i].Team_Name.toUpperCase()) {
+        checked = 0;
+        break;
+      }
+    }
+    if (checked == 1) {
+      try{
+        loadingScreen2.classList.remove("active");
+        loadingScreen1.classList.add("active");
+        const sukham = await fetch(scriptURL, {
+          method: "POST",
+          body: new FormData(form),
+        });
+        const result = await sukham.json();
+        if (result?.result == "success") {
+          console.log("Success!");
+          loadingScreen1.classList.remove("active");
+          SuccessPage.classList.add("Success");
+          form.reset();
+        }
+      }catch(error){
+        loadingScreen1.classList.remove("active");
+        loadingScreen2.classList.remove("active");
+        console.log("Error : ", error);
+        alert(
+          "Seems Heavy Traffic for registration . Please refresh the page and try once again"
+        );
+        form.reset();
+      }
+    } else {
+      loadingScreen2.classList.remove("active");
+      reEnter.classList.add("Success");
+    }
+  } catch (err) {
+    loadingScreen1.classList.remove("active");
+    loadingScreen2.classList.remove("active");
+    console.log("Error : ", err);
+    alert(
+      "Seems Heavy Traffic for registration . Please refresh the page and try once again"
+    );
+    form.reset();
+  }
+}
